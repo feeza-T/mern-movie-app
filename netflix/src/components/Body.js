99144013abe7cd07f6dfe2848dd1dbdc2761d689
Turna-Majumder <parbani.turna@gmail.com>
2024-06-29 10:34:10 +0600

    Search page

--- netflix/src/components/Body.js
+++ netflix/src/components/Body.js
@@ -2,6 +2,41 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './Login';
 import Browse from './Browse';
+import Search from './Search';
+import MovieDetail from './MovieDetail';
+
+const staticMovies = [
+  {
+    id: 1,
+    name: 'Inception',
+    description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
+    imageUrl: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p7825626_p_v8_ae.jpg',
+  },
+  {
+    id: 2,
+    name: 'Interstellar',
+    description: 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\'s population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind\'s new home.',
+    imageUrl: 'https://ebertfest.com/sites/default/files/large_lbGGuk9K1kNQqDabaMyFz1L9iTg.jpg',
+  },
+  {
+    id: 3,
+    name: 'Predator',
+    description: 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\'s population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind\'s new home.',
+    imageUrl: 'https://ebertfest.com/sites/default/files/large_lbGGuk9K1kNQqDabaMyFz1L9iTg.jpg',
+  },
+  {
+    id: 4,
+    name: 'The Martian',
+    description: 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\'s population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind\'s new home.',
+    imageUrl: 'https://ebertfest.com/sites/default/files/large_lbGGuk9K1kNQqDabaMyFz1L9iTg.jpg',
+  },
+  {
+    id: 5,
+    name: 'Matrix',
+    description: 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\'s population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind\'s new home.',
+    imageUrl: 'https://ebertfest.com/sites/default/files/large_lbGGuk9K1kNQqDabaMyFz1L9iTg.jpg',
+  },
+];
 
 const ErrorBoundary = ({ error }) => {
   return <div>Error: {error.message}</div>;
@@ -17,6 +52,16 @@ const Body = () => {
     {
       path: '/browse',
       element: <Browse />,
+      errorElement: <ErrorBoundary />
+    },
+    {
+      path: '/search',
+      element: <Search />,
+      errorElement: <ErrorBoundary />
+    },
+    {
+      path: '/movie/:id',
+      element: <MovieDetail movies={staticMovies} />,
       errorElement: <ErrorBoundary />
     }
   ]);
--- netflix/src/components/Header.js
+++ netflix/src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { IoIosArrowDropdown } from "react-icons/io";
+import { useNavigate, useLocation } from 'react-router-dom'; // Import useLocation
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation(); // Get current location
+
+  const handleHomeButtonClick = () => {
+    console.log("Home button clicked"); // Debug log
+    navigate('/');
+  }
+
   return (
-    <div className='absolute z-10 flex w-[100%] items-center justify-between px-6 bg-gradient-to-b from-black'>
+    <div className='absolute z-10 flex w-full items-center justify-between px-6 bg-gradient-to-b from-black'>
       <div className='flex items-center'>
         <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="netflix-logo" />
         <button className='bg-red-800 text-white px-4 py-2 ml-4'>My Lists</button>
@@ -13,7 +22,11 @@ const Header = () => {
         <h1 className='text-lg font-medium text-white'>Hello</h1>
         <div className='ml-4'>
           <button className='bg-red-800 text-white px-4 py-2'>Logout</button>
-          <button className='bg-red-800 text-white px-4 py-2 ml-2'>Search Movie</button>
+          {location.pathname === '/search' ? (
+            <button onClick={handleHomeButtonClick} className='bg-red-800 text-white px-4 py-2 ml-2'>Home</button>
+          ) : (
+            <button onClick={() => navigate('/search')} className='bg-red-800 text-white px-4 py-2 ml-2'>Search</button>
+          )}
         </div>
       </div>
     </div>
