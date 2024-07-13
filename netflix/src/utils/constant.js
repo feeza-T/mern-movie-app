2a6f42d023fb1e3617c4ad936ea5295835967e9b
feeza-T <takwafeeza84@gmail.com>
2024-07-14 00:02:26 +0600

    removeNodeModules

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,5 +1,8 @@
 
-export const API_END_POINT = "https://mern-movie-app-api-roan.vercel.app/api/v1/user";
+// export const API_END_POINT = "https://mern-movie-app-api-roan.vercel.app/api/v1/user";
+
+export const API_END_POINT = "https://localhost:8080/api/v1/user";
+
 
 export const options = {
     method: 'GET',
@@ -14,5 +17,4 @@ export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
 export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
 
 export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
-
 export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
